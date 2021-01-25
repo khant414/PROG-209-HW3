637c1d955dd99f3660dc764f6d78f368eb46af9f
khant414 <36459326+khant414@users.noreply.github.com>
2021-01-25 10:02:06 -0800

    adding HW Files to Github

---
+++ main.js
@@ -0,0 +1,29 @@
+customerArray = [];
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    
+});
+
+function StartProgram() {
+    let ul = document.getElementById("output");
+    ul.innerHTML = '';
+    
+    let Customer = {
+        name:  document.getElementById("name").value,
+        city: document.getElementById("city").value,
+        paymentType : document.getElementById("paymentType").value
+    }    
+
+    customerArray.push(Customer); 
+    console.log(customerArray.length)
+    customerArray.forEach(element => {
+        let li = document.createElement("li");
+        li.innerText = `${element.name} ${element.city} ${element.paymentType}` 
+        let ul = document.getElementById("output");
+        ul.appendChild(li)
+    });
+
+}
+    
+    
+
